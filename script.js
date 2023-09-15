13ecc6809937ab2969e53172931b78b80c9d3500
julioquintero21 <144246320+julioquintero21@users.noreply.github.com>
2023-09-15 08:09:28 -0300

    Versão 1.0
    Primeira Versão da Atividade/Projeto

---
+++ script.js
@@ -0,0 +1,104 @@
+
+    
+    function BarbieMovie() {
+
+        const myJSON_filmes = ["Barbie", "20 de Julho de 2023", "Greta Gerwig", "US$ 1,31 Bilhão", "Barbie e Ken"];
+        console.log(myJSON_filmes);
+    
+        document.getElementById("Sobre").innerHTML =
+            '<hr>'+
+                "<p>Filme: " + myJSON_filmes[0]  +  "<p>Data De Lançamento: " + myJSON_filmes[1] + "</p>" +
+                    "<p>Diretora: " + myJSON_filmes[2]  + "<p>Dolares Arrecadados: " + myJSON_filmes[3] + "</p>" +
+                    "<p>Personagem Principal: " + myJSON_filmes[4] + "</p>" + 
+                 "<div class='img2'><img src='imagens/barbiefotoclick.jpg' alt=''style='width:100%'>" + "</div>" +
+               '<div class="iframes" ><iframe  width="560" height="315"src="https://www.youtube.com/embed/NVIpIMqeJVM" title="Barbie | Trailer Oficial" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div> "</div>'
+            
+      //BARBIE Button      
+    }
+
+    function AvengersMovie() {
+        const myJSON_filmes = ["Avengers Endgame", "25 de Abril de 2019", "Irmãos Russo", "US$ 2,798 Bilhões", "Vingadores"];
+        console.log(myJSON_filmes);
+    
+        document.getElementById("Sobre").innerHTML =
+        '<hr>'+
+            "<p>Filme: " + myJSON_filmes[0] + "</p>" + "<p>Data De Lançamento: " + myJSON_filmes[1] + "</p>" +
+            "<p>Diretora: " + myJSON_filmes[2] + "</p>" +"<p>Dolares Arrecadados: " + myJSON_filmes[3] + "</p>" +
+            "<p>Personagem Principal: " + myJSON_filmes[4] + "</p>" +
+
+            "<div class='img2'><img src='imagens/vingafotoclick.jpg' alt=''style='width:100%'>" + "</div>" +
+            '<div class="iframes"><iframe width="560" height="315" src="https://www.youtube.com/embed/TcMBFSGVi1c" title="Marvel Studios&#39; Avengers: Endgame - Official Trailer" frameborder="0"allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>"</div>'
+          
+    }
+
+    function BlueBeatleMovie() {
+        const myJSON_filmes = ["Besouro Azul", "17 de agosto de 2023 ", "Angel Manoel Soto", "US$ 43,3 Milhões", "Jaime Reyes e Jenny "];
+        console.log(myJSON_filmes);
+    
+        document.getElementById("Sobre").innerHTML =
+        '<hr>'+
+            "<p>Filme: " + myJSON_filmes[0] + "</p>" + "<p>Data De Lançamento: " + myJSON_filmes[1] + "</p>" +
+            "<p>Diretora: " + myJSON_filmes[2] + "</p>" + "<p>Dolares Arrecadados: " + myJSON_filmes[3] + "</p>" +
+            "<p>Personagem Principal: " + myJSON_filmes[4] + "</p>" +
+            "<div class='img2'><img src='imagens/bluefotoclick.jpg' alt=''style='width:100%'>" + "</div>" +
+             '<div class="iframes"><iframe width="560" height="315" src="https://www.youtube.com/embed/WvAdCYosyVo?si=3Uum5jx2X7pbqPSK title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> "</div>'
+    
+                //BLUE Button//
+            }
+            function Blue() {
+                "<div class='img2'><img src='imagens/bluefotoclick.jpg' alt=''style='width:100%'>"  
+            }
+
+    function TWDMovie() {
+        const myJSON_filmes = ["The Walking Dead", "19 de Dezembro de 2016", "Elí Jorne", "US$ 609,4 Milhões", "Rick Grimes e Grupo "];
+        console.log(myJSON_filmes);
+    
+        document.getElementById("Sobre").innerHTML =
+        '<hr>'+
+            "<p>Filme: " + myJSON_filmes[0] + "</p>" + "<p>Data De Lançamento: " + myJSON_filmes[1] + "</p>" +
+            "<p>Diretora: " + myJSON_filmes[2] + "</p>" +"<p>Dolares Arrecadados: " + myJSON_filmes[3] + "</p>" +
+            "<p>Personagem Principal: " + myJSON_filmes[4] + "</p>" +
+
+            "<div class='img2'><img src='imagens/twdfotoclick.jpg' alt=''style='width:100%'>"  + "</div>" +
+            '<div class="iframes"><iframe width="560" height="315" src="https://www.youtube.com/embed/qAS8f9L-vWc" title"="The Walking Dead | Trailer Oficial da Última Temporada | FOX" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>"</div>'
+   
+             //TWD Button //
+        }
+
+    function OBXMovie() {
+        const myJSON_filmes = ["Outer Banks", "15 de Abril de 2020", "Jonas Pate", "US$ 400 Milhões.", "John B e os Pogues"];
+        console.log(myJSON_filmes);
+    
+        document.getElementById("Sobre").innerHTML =
+        '<hr>'+
+        "<p>Filme: " + myJSON_filmes[0] + "</p>" +"<p>Data De Lançamento: " + myJSON_filmes[1] + "</p>" +
+            "<p>Diretora: " + myJSON_filmes[2] + "</p>" + "<p>Dolares Arrecadados: " + myJSON_filmes[3] + "</p>" +
+            "<p>Personagem Principal: " + myJSON_filmes[4] + "</p>" +
+
+            "<div class='img2'><img src='imagens/obxfotoclick.jpg' alt=''style='width:100%'>"  + "</div>" +
+            '<div class="iframes"><iframe width="560"height="315" src="https://www.youtube.com/embed/CjzsoFglJ7w?si=q4iT-2wqZWUo52D2" title="YouTube video player" frameborder="0"allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>"</div>'
+    
+             //OBX Button // 
+        }
+
+    function TITASMovie() {
+        const myJSON_filmes = ["Titãs", "12 de Outubro de 2018", "Akiva Goldsman", "US$ 657,9 Milhões", "Dick Grayson "];
+        console.log(myJSON_filmes);
+    
+        document.getElementById("Sobre").innerHTML =
+             '<hr>'+
+            "<p>Filme: " + myJSON_filmes[0] + "</p>" + "<p>Data De Lançamento: " + myJSON_filmes[1] + "</p>" +
+            "<p>Diretora: " + myJSON_filmes[2] + "</p>" +"<p>Dolares Arrecadados: " + myJSON_filmes[3] + "</p>" +
+            "<p>Personagem Principal: " + myJSON_filmes[4] + "</p>" +
+
+            "<div class='img2'><img src='imagens/titansfotoclick.jpg' alt=''style='width:100%'>"  + "</div>" +
+           '<div class="iframes"><iframe width="560" height="315" src="https://www.youtube.com/embed/vPib9KwCBGs?si=OOZYR8gVmSL5hfpI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>"</div>'
+    
+                 //TITAS Button //
+        }
+
+
+    
+
+
+
